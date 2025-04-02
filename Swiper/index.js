7b8932949f2da9f41ab497f2651b1111513aef45
StReLoKD <7novak.danil7@gmail.com>
2025-04-02 15:19:44 +0300

    Add files via upload

--- Swiper/index.js
+++ Swiper/index.js
@@ -1,1 +1,40 @@
+new Swiper('.image-slider', {
+  // стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
 
+  // пэйджер
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    }
+  },
+
+  // переключение с клавиатуры
+  keyboard: {
+    enable: true,
+    onlyInViewport: true,
+  },
+
+  slidesPerView: 3, // слайдов для показа
+  slidesPerGroup: 1, // колво пролистываемых слайдов
+  centeredSlides: true, // активный слайд по центру
+  loop: true, // бесконечная прокрутка
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+    1440: {
+      slidesPerView: 3,
+    }
+  }
+});
